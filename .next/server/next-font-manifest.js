b7845495d3912d36dd35913c222ca609426729f8
kshetritej <tejghartikshetri@gmail.com>
2025-04-21 15:37:30 +0545

    add: builds

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/home/tj/Projects/mental/src/app/layout":["static/media/a34f9d1faa5f3315-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
